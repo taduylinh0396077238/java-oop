48540b9cef970013da77f10291eb661d6169890a
Duy Linh <cudotn111@gmail.com>
2022-04-07 00:32:28 +0700

    Duy Linh

--- java14/src/TestLine.java
+++ java14/src/TestLine.java
@@ -34,5 +34,19 @@ public class TestLine {
         System.out.println("end's y is: " + l1.getEndY());
         //end's y is: 24
 
+        l1.setBeginXY(31,32);
+        l2.setEndXY(33,34);
+        System.out.println(l1);  // Line's toString()
+        //Line[begin=(31,32),end=(33,34)]
+        System.out.println("begin's x and y are: " + Arrays.toString(l1.getBeginXY()));
+        //begin's x and y are: [31, 32]
+        System.out.println("end's x and y are: " + Arrays.toString(l1.getEndXY()));
+        //end's x and y are: [33, 34]
+
+        // Test getLength()
+        System.out.printf("length is: %.2f%n", l1.getLength());
+        //length is: 2.83
+
+
     }
 }
